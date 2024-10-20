a4d5fac95064f7d4b3e6b8ac14087ec17ef644e5
Futuremotion <futuremotionrecovery@gmail.com>
2024-10-19 20:53:10 -0500

    Tweaked build script.

--- Build.ps1
+++ Build.ps1
@@ -13,14 +13,5 @@ $Params2 = 'build', '--configuration', 'Debug', $Solution
 $BuildRelease = "$PSScriptRoot\src\bin\Release\net8.0"
 $BuildDebug = "$PSScriptRoot\src\bin\Debug\net8.0"
 
-# $VSYSModuleDirectory = "D:\Dev\Powershell\VSYSModules\FMFlattenFolders"
-# if(Test-Path -LiteralPath $VSYSModuleDirectory){
-#     Remove-Item -LiteralPath $VSYSModuleDirectory -Force -Recurse | Out-Null
-# }
-# New-Item -Path $VSYSModuleDirectory -ItemType Directory -Force | Out-Null
-
-# Get-ChildItem -Path $BuildRelease -File | Copy-Item -Destination $VSYSModuleDirectory -Recurse -Force | Out-Null
-# Set-Location -LiteralPath $VSYSModuleDirectory
-
 explorer.exe $BuildRelease
 
--- src/add-dependencies.cmd
+++ src/add-dependencies.cmd
@@ -1,3 +1,4 @@
 cd /D "%~dp0"
 dotnet add package PowerShellStandard.Library --version 7.0.0-preview.1
+dotnet add package System.Security.Permissions --version 8.0.0
 pause
